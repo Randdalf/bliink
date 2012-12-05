cd5c105e8b0bbc817657302a218a71c7de0d22c8
Coalzy <ac0541@bris.ac.uk>
2012-12-05 15:37:22 +0000

    Added basic AI test

---
+++ bliink_src/game/server/bliink_simple_npc.cpp
@@ -0,0 +1,176 @@
+//========= Copyright © 1996-2005, Valve Corporation, All rights reserved. ============//
+// This is a skeleton file for use when creating a new 
+// NPC. Copy and rename this file for the new
+// NPC and add the copy to the build.
+//
+// Leave this file in the build until we ship! Allowing 
+// this file to be rebuilt with the rest of the game ensures
+// that it stays up to date with the rest of the NPC code.
+//
+// Replace occurances of CBliinkSimpleNPC with the new NPC's
+// classname. Don't forget the lower-case occurance in 
+// LINK_ENTITY_TO_CLASS()
+//
+//
+// ASSUMPTIONS MADE:
+//
+// You're making a character based on CAI_BaseNPC. If this 
+// is not true, make sure you replace all occurances
+// of 'CAI_BaseNPC' in this file with the appropriate 
+// parent class.
+//
+// You're making a human-sized NPC that walks.
+//
+//=============================================================================//
+#include "cbase.h"
+#include "ai_default.h"
+#include "ai_task.h"
+#include "ai_schedule.h"
+#include "ai_hull.h"
+#include "soundent.h"
+#include "game.h"
+#include "npcevent.h"
+#include "entitylist.h"
+#include "activitylist.h"
+#include "ai_basenpc.h"
+#include "engine/IEngineSound.h"
+
+// memdbgon must be the last include file in a .cpp file!!!
+#include "tier0/memdbgon.h"
+
+//=========================================================
+// Private activities
+//=========================================================
+int	ACT_BLIINKCUSTOMACTIVITY = -1;
+
+//=========================================================
+// Custom schedules
+//=========================================================
+enum
+{
+	SCHED_BLIINKCUSTOMSCHEDULE = LAST_SHARED_SCHEDULE,
+};
+
+//=========================================================
+// Custom tasks
+//=========================================================
+enum 
+{
+	TASK_BLIINKCUSTOMTASK = LAST_SHARED_TASK,
+};
+
+
+//=========================================================
+// Custom Conditions
+//=========================================================
+enum 
+{
+	COND_BLIINKCUSTOMCONDITION = LAST_SHARED_CONDITION,
+};
+
+
+//=========================================================
+//=========================================================
+class CBliinkSimpleNPC : public CAI_BaseNPC
+{
+	DECLARE_CLASS( CBliinkSimpleNPC, CAI_BaseNPC );
+
+public:
+	void	Precache( void );
+	void	Spawn( void );
+	Class_T Classify( void );
+
+	DECLARE_DATADESC();
+
+	// This is a dummy field. In order to provide save/restore
+	// code in this file, we must have at least one field
+	// for the code to operate on. Delete this field when
+	// you are ready to do your own save/restore for this
+	// character.
+	int		m_iDeleteThisField;
+
+	DEFINE_CUSTOM_AI;
+};
+
+LINK_ENTITY_TO_CLASS( npc_bliink_simple, CBliinkSimpleNPC );
+IMPLEMENT_CUSTOM_AI( npc_citizen,CBliinkSimpleNPC );
+
+
+//---------------------------------------------------------
+// Save/Restore
+//---------------------------------------------------------
+BEGIN_DATADESC( CBliinkSimpleNPC )
+
+	DEFINE_FIELD( m_iDeleteThisField, FIELD_INTEGER ),
+
+END_DATADESC()
+
+//-----------------------------------------------------------------------------
+// Purpose: Initialize the custom schedules
+// Input  :
+// Output :
+//-----------------------------------------------------------------------------
+void CBliinkSimpleNPC::InitCustomSchedules(void) 
+{
+	INIT_CUSTOM_AI(CBliinkSimpleNPC);
+
+	ADD_CUSTOM_TASK(CBliinkSimpleNPC,		TASK_BLIINKCUSTOMTASK);
+
+	ADD_CUSTOM_SCHEDULE(CBliinkSimpleNPC,	SCHED_BLIINKCUSTOMSCHEDULE);
+
+	ADD_CUSTOM_ACTIVITY(CBliinkSimpleNPC,	ACT_BLIINKCUSTOMACTIVITY);
+
+	ADD_CUSTOM_CONDITION(CBliinkSimpleNPC,	COND_BLIINKCUSTOMCONDITION);
+}
+
+//-----------------------------------------------------------------------------
+// Purpose: 
+//
+//
+//-----------------------------------------------------------------------------
+void CBliinkSimpleNPC::Precache( void )
+{
+	PrecacheModel( "models/player/red_player.mdl" );
+
+	BaseClass::Precache();
+}
+
+
+//-----------------------------------------------------------------------------
+// Purpose: 
+//
+//
+//-----------------------------------------------------------------------------
+void CBliinkSimpleNPC::Spawn( void )
+{
+	Precache();
+
+	SetModel( "models/player/red_player.mdl" );
+	SetHullType(HULL_HUMAN);
+	SetHullSizeNormal();
+
+	SetSolid( SOLID_BBOX );
+	AddSolidFlags( FSOLID_NOT_STANDABLE );
+	SetMoveType( MOVETYPE_STEP );
+	SetBloodColor( BLOOD_COLOR_RED );
+	m_iHealth			= 20;
+	m_flFieldOfView		= 0.5;
+	m_NPCState			= NPC_STATE_NONE;
+
+	CapabilitiesClear();
+	//CapabilitiesAdd( bits_CAP_NONE );
+
+	NPCInit();
+}
+
+
+//-----------------------------------------------------------------------------
+// Purpose: 
+//
+//
+// Output : 
+//-----------------------------------------------------------------------------
+Class_T	CBliinkSimpleNPC::Classify( void )
+{
+	return	CLASS_NONE;
+}
